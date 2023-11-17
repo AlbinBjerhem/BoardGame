25497cf8100b46b32abb77b99c30554d23837858
AlbinBjerhem <albinbjerhem@gmail.com>
2023-11-17 16:44:55 +0100

    Fixat SAP
    Ejj bra må tilläggas xD

---
+++ JS/routing.js
@@ -0,0 +1,11 @@
+function navigateToRegistration() {
+  window.location.href = '../HTML/UserRegistration.html';
+}
+
+function navigateToGame() {
+  window.location.href = '../HTML/GamePage.html';
+}
+
+function navigateToScores() {
+  window.location.href = '../HTML/ScorePage.html';
+}
